683390ff24d1963a8a4657a77153bab01f948fc5
Kush <kushpatel124@gmail.com>
2022-12-06 21:28:56 -0500

    commit tree afdb5dd18fe3cac8ffd0d66a7360b18bae8d781c

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(fixed = "roller-skate") {
+  return (`This Saturday, I want to ${fixed}!`);
+}
+saturdayFun('bathe my dog');
+
+function mondayWork(blank = "go to the office") {
+  return (`This Monday, I will ${blank}.`);
+}
+mondayWork("take PTO");
+
+let wrapAdjective = function (special = "*") {
+  return function (adj = "a hard worker",) {
+    return `You are ${special}${adj}${special}!`;
+  };
+};
+wrapAdjective("||")("a dedicated programmer");
